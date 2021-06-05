5afe339cf93b44fdc6313c80f8883f48d910aa73
hamzsait <hamzasait@utexas.edu>
2021-06-05 16:27:27 -0500

    converted webdriver to module

--- twitterStats.py
+++ twitterStats.py
@@ -1,22 +1,5 @@
-import os
-from os import environ
-from selenium import webdriver
+from webdriver import getWebdriver
 from time import sleep
-
-def getWebdriver(local = False):
-
-    if local:
-        return webdriver.Chrome()
-
-    op = webdriver.ChromeOptions()
-    op.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-    op.add_argument("--headless")
-    op.add_argument("--no-sandbox")
-    op.add_argument("--disable-dec-sh-usage")
-
-    driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), options=op)
-
-    return driver
 
 def twitterCongress(link, local = False):
 
